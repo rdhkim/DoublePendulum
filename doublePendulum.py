fb0bc06b3e82c2b0c09e6d8a37fd435cc1d6b260
Ryan Kim <rdhkim0@gmail.com>
2022-03-10 23:30:20 -0500

    first commit

---
+++ doublePendulum.py
@@ -0,0 +1,7 @@
+import numpy as np
+import sympy as smp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import PillowWriter
